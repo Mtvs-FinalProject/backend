f7bb7cdd5b9d1885be0ed787a0394efcaa5af421
apfp77 <apfp77@naver.com>
2024-10-08 10:37:18 +0900

    init

---
+++ src/main/java/org/block/blockbackend2/BlockBackend2Application.java
@@ -0,0 +1,13 @@
+package org.block.blockbackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockBackend2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockBackend2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/block/blockbackend2/BlockBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.block.blockbackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockBackend2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
